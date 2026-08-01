df497e5eb01fdef9d05263486c91e60a521657de
24Mark8 <markmundy@tiscali.co.uk>
2026-08-01 17:13:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -120,22 +120,22 @@ export default function Home() {
 
   <div className="mt-12 grid gap-8 text-center md:grid-cols-4">
     <div>
-      <h3 className="text-4xl font-extrabold text-blue-700">24+</h3>
+      <h3 className="text-5xl font-extrabold text-blue-700">24+</h3>
       <p className="mt-2 text-slate-600">Years in the NHS</p>
     </div>
 
     <div>
-      <h3 className="text-4xl font-extrabold text-blue-700">100+</h3>
+      <h3 className="text-5xl font-extrabold text-blue-700">100+</h3>
       <p className="mt-2 text-slate-600">Power BI Dashboards</p>
     </div>
 
     <div>
-      <h3 className="text-4xl font-extrabold text-blue-700">1M+</h3>
+      <h3 className="text-5xl font-extrabold text-blue-700">1M+</h3>
       <p className="mt-2 text-slate-600">Records Analysed</p>
     </div>
 
     <div>
-      <h3 className="text-4xl font-extrabold text-blue-700">24/7</h3>
+      <h3 className="text-5xl font-extrabold text-blue-700">24/7</h3>
       <p className="mt-2 text-slate-600">Decision Support</p>
     </div>
   </div>
